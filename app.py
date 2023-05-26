ff299b5752762203a79e1f124bd61b90e4844b36
Sandip Pal <palsa@flip2.engr.oregonstate.edu>
2023-05-25 21:48:09 -0700

    basic working site

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
 import os
 # import database.db_connector as db
 
@@ -7,6 +9,14 @@ import os
 app = Flask(__name__, template_folder='htmls')
 
 # db_connection = db.connect_to_database()
+
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_palsa'
+app.config['MYSQL_PASSWORD'] = '5384' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_palsa'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -31,4 +41,3 @@ def page4():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 9112))
     app.run(port=port, debug=True)
-
