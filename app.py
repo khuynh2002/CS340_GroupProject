9912266752c58cac75410e2f674d91229bd81473
Sandip Pal <palsa@flip2.engr.oregonstate.edu>
2023-05-25 22:51:10 -0700

    So far, we've replaced index.html render_template with placeholder main.j2, and route /pcps does pull from our database, but we need to get the templating working so the data gets embedded into html. Along with our other crud operations

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
 import os
-# import database.db_connector as db
+import database.db_connector as db
 
 # Configuration
 
 app = Flask(__name__, template_folder='htmls')
 
-# db_connection = db.connect_to_database()
+db_connection = db.connect_to_database()
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
 app.config['MYSQL_USER'] = 'cs340_palsa'
@@ -20,19 +20,25 @@ mysql = MySQL(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.j2')
 
-@app.route('/pcps.html')
+@app.route('/locations')
 def page1():
     return render_template('locations.html')
 
-@app.route('/patients.html')
+@app.route('/patients')
 def page2():
     return render_template('patients.html')
 
-@app.route('/pcps.html')
+@app.route('/pcps')
 def page3():
-    return render_template('pcps.html')
+    query = "SELECT * FROM PCPs;"
+
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    results = json.dumps(cursor.fetchall())
+    return results
+    #return render_template('pcps.html')
 
 @app.route('/visits.html')
 def page4():
