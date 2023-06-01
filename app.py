8f8b65d1e160d1532baf11e301ed24121ff9da14
KEVINS\19717 <kevinhuynh200289@gmail.com>
2023-06-01 15:00:18 -0700

    CRUD functionality for pcps

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ import database.db_connector as db
 
 app = Flask(__name__, template_folder='htmls')
 
-db_connection = db.connect_to_database()
-
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
 app.config['MYSQL_USER'] = 'cs340_palsa'
 app.config['MYSQL_PASSWORD'] = '5384' #last 4 of onid
@@ -17,6 +15,7 @@ app.config['MYSQL_DB'] = 'cs340_palsa'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
+db_connection = db.connect_to_database()
 
 @app.route('/')
 def home():
@@ -30,15 +29,46 @@ def page1():
 def page2():
     return render_template('patients.html')
 
-@app.route('/pcps')
-def page3():
+class PCP():
+    id = mysql.Column(mysql.Integer, primary_key=True)
+
+
+@app.route('/pcps', methods=['GET', 'POST'])
+def pcps():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name') 
+        last_name = request.form.get('last_name')
+        pcp_specialty = request.form.get('pcp_specialty')
+        
+        query = "INSERT INTO PCPs (first_name, last_name, pcp_specialty) VALUES (%s, %s, %s);"
+        db.execute_query(db_connection=db_connection, query=query, query_params=(first_name, last_name, pcp_specialty))
+
+
+    #sample code
     query = "SELECT * FROM PCPs;"
-
     cursor = db.execute_query(db_connection=db_connection, query=query)
-
     results = json.dumps(cursor.fetchall())
     return results
-    #return render_template('pcps.html')
+    return render_template("pcps.j2", pcps=results)
+
+@app.route('/pcps/<int:pcp_id>', methods=['GET', 'POST', 'DELETE'])
+def pcp(pcp_id):
+    if request.method == 'POST':
+        first_name = request.form.get('first_name') 
+        last_name = request.form.get('last_name')
+        pcp_specialty = request.form.get('pcp_specialty')
+        
+        query = "UPDATE PCPs SET first_name = %s, last_name = %s, pcp_specialty = %s WHERE pcp_id = %s;"
+        db.execute_query(db_connection=db_connection, query=query, query_params=(first_name, last_name, pcp_specialty, pcp_id))
+
+    elif request.method == 'DELETE':
+        query = "DELETE FROM PCPs WHERE pcp_id = %s;"
+        db.execute_query(db_connection=db_connection, query=query, query_params=(pcp_id,))
+        
+    query = "SELECT * FROM PCPs WHERE pcp_id = %s;"
+    cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(pcp_id,))
+    result = cursor.fetchone()
+    return render_template("pcp.j2", pcp=result)  # Render a specific template for individual PCP
 
 @app.route('/visits.html')
 def page4():
