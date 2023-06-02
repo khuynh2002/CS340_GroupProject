a1e7873f5e1b5f687275075677e9baa3f475e015
Sandip Pal <palsa@flip2.engr.oregonstate.edu>
2023-06-01 21:52:58 -0700

    Got Locations CRUD working, so both PCPs and Locations have full CRUD capabilities

--- app.py
+++ app.py
@@ -21,10 +21,6 @@ db_connection = db.connect_to_database()
 def home():
     return render_template('main.j2')
 
-@app.route('/locations')
-def page1():
-    return render_template('locations.html')
-
 @app.route('/patients')
 def page2():
     return render_template('patients.html')
@@ -55,12 +51,6 @@ def delete_pcp(pcp_id):
     return redirect('/pcps')
 
 
-    # #sample code
-    # query = "SELECT * FROM PCPs;"
-    # cursor = db.execute_query(db_connection=db_connection, query=query)
-    # results = cursor.fetchall()
-    # return render_template("pcps3.j2", pcps=results)
-
 @app.route('/pcps/<int:pcp_id>', methods=['GET', 'POST', 'DELETE'])
 def pcp(pcp_id):
     if request.method == 'POST':
@@ -79,6 +69,49 @@ def pcp(pcp_id):
     cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(pcp_id,))
     result = cursor.fetchone()
     return render_template("pcp.j2", pcp=result)  # Render a specific template for individual PCP
+
+@app.route('/locations', methods=['GET', 'POST'])
+def locations():
+    if request.method == 'POST':
+        location_name = request.form.get('location_name') 
+        location_city = request.form.get('location_city')
+        location_state = request.form.get('location_state')
+        location_zip = request.form.get('location_zip')
+        
+        query = "INSERT INTO Locations (location_name, location_city, location_state, location_zip) VALUES (%s, %s, %s, %s);"
+        db.execute_query(db_connection=db_connection, query=query, query_params=(location_name, location_city, location_state, location_zip))
+        return redirect('/locations')
+    #sample code
+    query = "SELECT * FROM Locations;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("locations.j2", locations=results)
+
+@app.route('/delete_location/<int:location_id>', methods=['GET', 'POST'])
+def delete_location(location_id): 
+    query = "DELETE FROM Locations WHERE location_id = %s;"
+    cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(location_id,))
+    return redirect('/locations')
+
+@app.route('/locations/<int:location_id>', methods=['GET', 'POST', 'DELETE'])
+def location(location_id):
+    if request.method == 'POST':
+        location_name = request.form.get('location_name') 
+        location_city = request.form.get('location_city')
+        location_state = request.form.get('location_state')
+        location_zip = request.form.get('location_zip')
+        
+        query = "UPDATE Locations SET location_name = %s, location_city = %s, location_state = %s, location_zip = %s WHERE location_id = %s;"
+        db.execute_query(db_connection=db_connection, query=query, query_params=(location_name, location_city, location_state, location_zip, location_id))
+
+    elif request.method == 'DELETE':
+        query = "DELETE FROM Locations WHERE location_id = %s;"
+        db.execute_query(db_connection=db_connection, query=query, query_params=(location_id,))
+        
+    query = "SELECT * FROM Locations WHERE location_id = %s;"
+    cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(location_id,))
+    result = cursor.fetchone()
+    return render_template("locations2.j2", location=result) 
 
 @app.route('/visits.html')
 def page4():
